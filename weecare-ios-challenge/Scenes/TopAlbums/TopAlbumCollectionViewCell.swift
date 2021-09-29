8c406baecf386d60f2879c27ca64a8166e5f3d93
enriqueflorencio <eflorencio@ucmerced.edu>
2021-09-29 03:10:13 -0700

    Added in unit tests for network and api requests. Also reorganized networking/api files into a new group called 'model'

--- weecare-ios-challenge/Scenes/TopAlbums/TopAlbumCollectionViewCell.swift
+++ weecare-ios-challenge/Scenes/TopAlbums/TopAlbumCollectionViewCell.swift
@@ -35,7 +35,7 @@ class TopAlbumCollectionViewCell: UICollectionViewCell {
     private func commonInit() {
         //MARK: TODO- Revert the background color back to the original once I'm done working on the feature. Also remember to revert the build target back to iOS 14.5 as well.
         albumImageView.backgroundColor = UIColor.lightGray.withAlphaComponent(0.5)
-//        albumImageView.contentMode = .scaleAspectFit
+        albumImageView.contentMode = .scaleAspectFit
         albumImageView.clipsToBounds = true
         albumImageView.layer.cornerRadius = 7
         albumImageView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
@@ -81,30 +81,12 @@ class TopAlbumCollectionViewCell: UICollectionViewCell {
             albumImageView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
             albumImageView.topAnchor.constraint(equalTo: containerView.topAnchor),
             albumImageView.heightAnchor.constraint(equalToConstant: 120),
-            albumImageView.widthAnchor.constraint(equalTo: containerView.widthAnchor),
+            albumImageView.widthAnchor.constraint(equalTo: containerView.heightAnchor),
             
             //Stack
             stackView.topAnchor.constraint(equalTo: albumImageView.bottomAnchor, constant: 4),
             stackView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 8),
             stackView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor)
-            
-            
-            
-//            albumImageView.widthAnchor.constraint(equalTo: containerView.widthAnchor),
-//            albumImageView.heightAnchor.constraint(equalTo: containerView.heightAnchor, multiplier: 0.6)
-
-            // ImageView
-//            albumImageView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
-//            albumImageView.topAnchor.constraint(equalTo: containerView.topAnchor),
-//            albumImageView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
-//            albumImageView.widthAnchor.constraint(equalToConstant: 100),
-//            albumHeight,
-//
-//            // Stack
-//            stackView.leadingAnchor.constraint(equalTo: albumImageView.trailingAnchor, constant: 10),
-//            stackView.topAnchor.constraint(equalTo: containerView.topAnchor),
-//            stackView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
-//            containerView.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: 10)
         ])
     }
 }
---
+++ weecare-ios-challengeTests/weecare_ios_challengeTests.swift
@@ -0,0 +1,73 @@
+//
+//  weecare_ios_challengeTests.swift
+//  weecare-ios-challengeTests
+//
+//  Created by Enrique Florencio on 9/29/21.
+//
+
+import XCTest
+@testable import weecare_ios_challenge
+
+class weecare_ios_challengeTests: XCTestCase {
+    private let network = Network(
+        sessionConfig: URLSessionConfiguration.default
+    )
+    private lazy var iTunesAPI : ITunesAPI = {
+        let api = ITunesAPI(network: network)
+        return api
+    }()
+    private var albumLimit = 10
+    private let expectationTimeout = 30.0
+    private var albums = [Album]()
+
+    override func setUp() {
+        continueAfterFailure = false
+    }
+    
+    func testGetRequest() {
+        let getAlbumExpectation = XCTestExpectation(description: "async album get request")
+        getTopAlbumsFromApi(expectation: getAlbumExpectation)
+        wait(for: [getAlbumExpectation], timeout: expectationTimeout)
+        
+        let getArtworkExpectation = XCTestExpectation(description: "async artwork get request")
+        getAlbumImageFromApi(expectation: getArtworkExpectation)
+        wait(for: [getArtworkExpectation], timeout: expectationTimeout)
+    }
+    
+    private func getTopAlbumsFromApi(expectation: XCTestExpectation) {
+        iTunesAPI.getTopAlbums(limit: albumLimit) { [weak self] result in
+            switch result {
+            case .success(let data):
+                guard let self = self else { return XCTFail()}
+                let resultCount = data.feed.results.count
+                XCTAssertEqual(resultCount, self.albumLimit, "The amount of albums fetched from the API are not equal to the amount we requested.")
+                self.albums = data.feed.results
+                expectation.fulfill()
+            case .failure(let err):
+                XCTFail("Get Top Albums Failed: \(err.localizedDescription)")
+            }
+            
+        }
+    }
+    
+    private func getAlbumImageFromApi(expectation: XCTestExpectation) {
+        if let albumArtworkURL = albums.first?.artworkUrl100 {
+            let apiRequest = APIRequest(url: albumArtworkURL)
+            network.requestData(apiRequest) { (result) in
+                switch result {
+                case .success(let data):
+                    XCTAssertNotNil(data, "The artwork fetched from the API is nil.")
+                    expectation.fulfill()
+                case .failure(let err):
+                    XCTFail("Get Album Artwork Failed: \(err.localizedDescription)")
+                }
+            }
+        }
+        
+    }
+    
+    
+    
+    
+
+}
